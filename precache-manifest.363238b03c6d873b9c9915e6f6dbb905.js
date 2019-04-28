04e29a09da5b7d39092ae8d08a6aeaa3255c168f
Alan Fidelino <afidedev26@gmail.com>
2019-04-28 14:52:34 +1000

    Updates

---
+++ precache-manifest.363238b03c6d873b9c9915e6f6dbb905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b58b3f37b939ac50369",
+    "url": "/af-resume/static/js/runtime~main.edbd56f8.js"
+  },
+  {
+    "revision": "3ace475bfd5791a124bb",
+    "url": "/af-resume/static/js/main.07680403.chunk.js"
+  },
+  {
+    "revision": "25581319dd46906beac5",
+    "url": "/af-resume/static/js/2.b8d73925.chunk.js"
+  },
+  {
+    "revision": "3ace475bfd5791a124bb",
+    "url": "/af-resume/static/css/main.1778e79b.chunk.css"
+  },
+  {
+    "revision": "7c5904d8d95f7a470a7811c9cb9cc763",
+    "url": "/af-resume/index.html"
+  }
+];
